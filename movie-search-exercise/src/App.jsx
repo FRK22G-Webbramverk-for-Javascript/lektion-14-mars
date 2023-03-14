c4247370a3b846d1aa61d5272a82657572c4aee6
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-03-14 13:48:03 +0100

    More comments

--- movie-search-exercise/src/App.jsx
+++ movie-search-exercise/src/App.jsx
@@ -5,11 +5,11 @@ import SearchMovies from './components/SearchMovies/SearchMovies';
 import DisplayMovies from './components/DisplayMovies/DisplayMovies';
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]); // Här sparas alla filmer från API-anropet för att kunna skickas ner till DisplayMovies
 
   return (
     <div className="App">
-      <SearchMovies updateMovies={ setMovies } />
+      <SearchMovies updateMovies={ setMovies } /> { /* Skickar ner setMovies som props för att kunna uppdatera movies */}
       <DisplayMovies movies={ movies } />
     </div>
   )
--- movie-search-exercise/src/components/SearchMovies/SearchMovies.jsx
+++ movie-search-exercise/src/components/SearchMovies/SearchMovies.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 
 function SearchMovies(props) {
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState(''); // Detta state används för att spara värdet från inputfältet
 
     async function handleClick() {
         const response = await fetch(`http://www.omdbapi.com/?apikey=37fe945a&s=${title}`);
         const data = await response.json();
         console.log(data);
 
+        // Filtrera ut enbart filmer (kan finnas spel med)
         const onlyMovies = data.Search.filter((movie) => {
             if (movie.Type === 'movie') {
                 return movie;
@@ -15,9 +16,9 @@ function SearchMovies(props) {
         });
 
         if (data.Response === 'True') {
-            props.updateMovies(onlyMovies);
+            props.updateMovies(onlyMovies); // Uppdatera movies i App med alla filmer från API-anropet
         } else {
-            props.updateMovies([]);
+            props.updateMovies([]); // Ifall inga filmer hittades sätt en tom array
         }
 
         // Ovan fast med en tenary operator om du dock har flera saker som ska se i en if-sats så använd ovan istället
